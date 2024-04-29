c51a6b840007d78767e1ca1e96c417255b443c3c
Thalles Ian <thallesyam@gmail.com>
2024-04-28 21:53:50 -0300

    feat: finish 3 module

---
+++ 03.module-concepts-tajs/06.e2e/src/person.js
@@ -0,0 +1,32 @@
+class Person {
+  static validate(person) {
+    if(!person.name) throw new Error('Name is required')
+    if(!person.cpf) throw new Error('CPF is required')
+  }
+
+  static format(person) {
+    const [name, ...lastName] = person.name.split(' ')
+    return {
+      cpf: person.cpf.replace(/\D/g, ''),
+      name,
+      lastName: lastName.join(' ')
+    }
+  }
+
+  static save(person) {
+    if(!['cpf', 'name', 'lastName'].every(prop => person[prop])) {
+      throw new Error(`Cannot save invalid person!`)
+    }
+
+    return 'Registrado com sucesso!!'
+  }
+
+  static process(person) {
+    this.validate(person)
+    const personFormated = this.format(person)
+    this.save(personFormated)
+    return 'ok'
+  }
+}
+
+export default Person
